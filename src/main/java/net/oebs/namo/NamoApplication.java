8423ebd02a8c152a5bb29e8b98b4756c433727f6
Oliver Seemann <oseemann@gmail.com>
2014-05-04 23:14:19 +0200

    Add create realm request, add plain db handling

--- src/main/java/net/oebs/namo/NamoApplication.java
+++ src/main/java/net/oebs/namo/NamoApplication.java
@@ -1,12 +1,18 @@
 package net.oebs.namo;
 
+import com.jolbox.bonecp.BoneCP;
+import com.jolbox.bonecp.BoneCPConfig;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import java.sql.SQLException;
 import net.oebs.namo.health.TemplateHealthCheck;
 import net.oebs.namo.resources.RealmResource;
 
 public class NamoApplication extends Application<NamoConfiguration> {
+
+    private BoneCP connectionPool;
+
     public static void main(String[] args) throws Exception {
         new NamoApplication().run(args);
     }
@@ -18,16 +24,28 @@ public class NamoApplication extends Application<NamoConfiguration> {
 
     @Override
     public void initialize(Bootstrap<NamoConfiguration> bootstrap) {
-        // nothing to do yet
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException ex) { }
+
+        BoneCPConfig config;
+        config = new BoneCPConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5433/namo");
+        config.setUsername("namo");
+        config.setPassword("namo");
+
+        try {
+            this.connectionPool = new BoneCP(config);
+        } catch (SQLException ex) { }
     }
 
     @Override
     public void run(NamoConfiguration configuration, Environment environment) {
-        final RealmResource realm = new RealmResource();
-        
-        final TemplateHealthCheck healthCheck =
-            new TemplateHealthCheck(configuration.getTemplate());
-                
+        final RealmResource realm = new RealmResource(this.connectionPool);
+
+        final TemplateHealthCheck healthCheck
+                = new TemplateHealthCheck(configuration.getTemplate());
+
         environment.jersey().register(realm);
         environment.healthChecks().register("template", healthCheck);
     }
--- src/main/java/net/oebs/namo/resources/RealmResource.java
+++ src/main/java/net/oebs/namo/resources/RealmResource.java
@@ -3,21 +3,54 @@ package net.oebs.namo.resources;
 import net.oebs.namo.core.Realm;
 import com.google.common.base.Optional;
 import com.codahale.metrics.annotation.Timed;
+import com.jolbox.bonecp.BoneCP;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import java.util.concurrent.atomic.AtomicLong;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @Path("/realm")
 @Produces(MediaType.APPLICATION_JSON)
 public class RealmResource {
 
+    private BoneCP connectionPooler;
+
+    public RealmResource(BoneCP connectionPooler) {
+        this.connectionPooler = connectionPooler;
+    }
+
     @GET
     @Timed
     public Realm getRealm(@QueryParam("realm_id") Optional<String> realmId) {
         return new Realm("foobar97");
+    }
+
+    @POST
+    @Timed
+    public Realm createRealm() {
+        Connection conn;
+        String realmId = null;
+        final String insert
+                = "INSERT INTO realm VALUES (default) RETURNING id";
+
+        try {
+            conn = this.connectionPooler.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(insert);
+            rs.next();
+            realmId = rs.getString("id");
+            conn.close();
+        } catch (SQLException ex) {
+        }
+
+        return new Realm(realmId);
     }
 }
